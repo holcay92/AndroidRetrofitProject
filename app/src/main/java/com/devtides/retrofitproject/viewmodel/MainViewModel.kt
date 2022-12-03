da9b5511e2a58875dfdafdbc2a9f11fb6ce3e66f
H.Halil Olcay <holcay92@gmail.com>
2022-12-03 18:08:27 +0300

    Update MainViewModel.kt

--- app/src/main/java/com/devtides/retrofitproject/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/devtides/retrofitproject/viewmodel/MainViewModel.kt
@@ -2,20 +2,18 @@ package com.devtides.retrofitproject.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.devtides.retrofitproject.model.Item
-import com.devtides.retrofitproject.model.TYPE_CATEGORY
-import com.devtides.retrofitproject.model.TYPE_ITEM
+import com.devtides.retrofitproject.model.*
 import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     var job: Job? = null
     val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
         GlobalScope.launch(Dispatchers.Main) {
-            onError("Exception: ${throwable.localizedMessage}")
+            onError("Exception: ${throwable.localizedMessage}"+" error!!")
         }
     }
 
@@ -24,16 +22,49 @@ class MainViewModel: ViewModel() {
     val error = MutableLiveData<String>()
 
     fun fetchData() {
-        apiResponse.value = arrayListOf(
-            Item("Category1", "", TYPE_CATEGORY),
-            Item("Key1", "Value1", TYPE_ITEM),
-            Item("Key2", "Value2", TYPE_ITEM),
-            Item("Category2", "", TYPE_CATEGORY),
-            Item("Key3", "Value3", TYPE_ITEM),
-            Item("Key4", "Value4", TYPE_ITEM)
-        )
-        error.value = null
-        loading.value = false
+        loading.value = true
+
+
+        // enqueue() is an asynchronous call
+        // enqueue() is a callback function
+        // enqueue() is a function that will be called when the response is ready
+        // main thread will not be blocked
+        // enqueue() will be called on a background thread
+
+        ApiCallService.getApiCall().enqueue(object : Callback<ApiCallResponse> {
+
+            override fun onResponse( getApiCall: Call<ApiCallResponse>, response: Response<ApiCallResponse> ) {
+                // response.body() is a function that will be called when the response is ready
+                // response.body() is a callback function
+                // response.body() is a function that will be called on a background thread
+                // main thread will not be blocked
+                //if response is successful then it will return the response body
+                val body:ApiCallResponse? = response.body()
+                apiResponse.value = body?.flatten()
+                error.value = null
+                loading.value = false
+            }
+            override fun onFailure(getApiCall: Call<ApiCallResponse>, t: Throwable) {
+                onError(t.localizedMessage+"asd")
+            }
+        })
+    }
+
+    fun fetchDataSync(){
+        loading.value = true
+
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val response = ApiCallService.getApiCall().execute()
+              withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        apiResponse.value = response.body()?.flatten()
+                        error.value = null
+                        loading.value = false
+                    } else {
+                        onError("Error : ${response.message()}")
+                    }
+                }
+        }
     }
 
     private fun onError(message: String) {
